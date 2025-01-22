5db61871d033e6a382a6a22faeaaf70f3583516e
yeejord <147876292+yeejord@users.noreply.github.com>
2025-01-22 15:41:53 -0500

    adding name to labs and git url

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -8,6 +8,7 @@ export default function Labs() {
     return (
         <div id="wd-labs">
             <h1>Labs</h1>
+            <h2>Jordyn Yee</h2>
             <TOC />
             <Routes>
                 <Route path="/" element = {<Navigate to="Lab1" />} />
